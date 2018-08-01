f3806180c6df02becfde0bef6a7862b76fa6b0a2
Holden Altaffer <haltaffer@gmail.com>
2018-08-01 18:42:32 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,6 @@ Rails.application.routes.draw do
    resources :students, only: [:index, :new, :create]
     get '/student/:id', to: 'students#show', as: 'student'
 
-    resources :school_classes, only: [:index, :new, :create]
+    resources :school_classes, only: [:index, :new, :create, :edit, :update]
      get '/school_class/:id', to: 'school_classes#show', as: 'school_class'
 end
